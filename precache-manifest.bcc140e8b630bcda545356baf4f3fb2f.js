8e051380f7990a54bbb35ac3832f669f38870827
craigmacclean <cmacc@hotmail.com>
2020-05-24 20:54:54 -0700

    Updates

---
+++ precache-manifest.bcc140e8b630bcda545356baf4f3fb2f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e8b9b06a20c9599b23c44f602130023",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "a100be31e3a0675486cf",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "a63154158079790bfa16",
+    "url": "/robofriends/static/css/main.745c8d6d.chunk.css"
+  },
+  {
+    "revision": "a100be31e3a0675486cf",
+    "url": "/robofriends/static/js/2.ca4a7502.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.ca4a7502.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a63154158079790bfa16",
+    "url": "/robofriends/static/js/main.0e5ae1a9.chunk.js"
+  },
+  {
+    "revision": "99557a09ab68caec6407",
+    "url": "/robofriends/static/js/runtime-main.229ccf3b.js"
+  }
+]);
